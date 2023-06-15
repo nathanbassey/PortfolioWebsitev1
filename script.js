ab4179d2ccdad33c8aa5857bc50aca1f1c2094b9
nathanbassey <nathanbassey@gmail.com>
2023-06-15 11:56:47 -0400

    Added javascript for AboutMe section

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ document.querySelector(".navbar-toggle").addEventListener("click", function () {
       links[i].classList.toggle("active");
     }
   });
+
+  var tablinks = document.getElementsByClassName("tab-links");
+  var tabcontents = document.getElementsByClassName("tab-contents");
+  
+  function opentab(tabname){
+    for(tablink of tablinks){
+      tablink.classList.remove("active-link")
+    }
+    for(tabcontent of tabcontents){
+      tabcontent.classList.remove("active-tab")
+    }
+    addEventListener.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+  }
