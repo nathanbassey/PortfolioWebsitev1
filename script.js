5c87b46f5b79bbf7d43005fdcb34ec9d74e20d5f
nathanbassey <nathanbassey@gmail.com>
2023-06-16 00:55:43 -0400

    Create portfolio section

--- script.js
+++ script.js
@@ -5,16 +5,31 @@ document.querySelector(".navbar-toggle").addEventListener("click", function () {
     }
   });
 
-  var tablinks = document.getElementsByClassName("tab-links");
-  var tabcontents = document.getElementsByClassName("tab-contents");
+  // let tablinks = document.getElementsByClassName("tab-links");
+  // let tabcontents = document.getElementsByClassName("tab-contents");
   
-  function opentab(tabname){
-    for(tablink of tablinks){
-      tablink.classList.remove("active-link")
-    }
-    for(tabcontent of tabcontents){
-      tabcontent.classList.remove("active-tab")
-    }
-    addEventListener.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-  }
+  // function opentab(tabname){
+  //   for(tablink of tablinks){
+  //     tablink.classList.remove("active-link")
+  //   }
+  //   for(tabcontent of tabcontents){
+  //     tabcontent.classList.remove("active-tab")
+  //   }
+  //   event.currentTarget.classList.add("active-link")
+  //   document.getElementById(tabname).classList.add("active-tab");
+  // } 
+
+// function opentab(evt, tabHead){
+//   let tabcontents = document.getElementsByClassName("tab-contents");
+//   for (i=0; i < tabcontents.length; i++) {
+//     tabcontents[i].style.display = "none"
+//   }
+
+//   let tablinks = document.getElementsByClassName("tab-links")
+//   for (i=0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.add("active-tab")
+//   }
+
+//   document.getElementsByClassName(tabHead).style.display = "block";
+//   evt.currentTarget.className += "active-link"
+// }
